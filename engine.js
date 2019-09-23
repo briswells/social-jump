2fa3b1849af950326ea96dad668f3d2bbfcf7b37
Brian Wells <Briswells@gmail.com>
2019-09-23 11:26:54 -0700

    The basic class framework for the game

---
+++ engine.js
@@ -0,0 +1,1 @@
+//will combine the data from the other classes and will trigger the a game frame to be drawn at a regular interval
---
+++ server.js
@@ -0,0 +1,1 @@
+//will be used to handle all websocket calls and update display based on other players controller classes.
